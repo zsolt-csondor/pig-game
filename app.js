44813affd01a7e8768e23b0604b966a856b997c0
zsolt-csondor <zsolt.csondor@gmail.com>
2018-03-29 16:44:20 +0100

    Added click event handler to button and dice display

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6 + 1);
 
-alert(dice); 
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+
+  var dice = Math.floor(Math.random() * 6 + 1);
+
+  var diceDOM = document.querySelector(".dice");
+
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
