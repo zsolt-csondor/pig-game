a7ac84aacd1d27135a18fa7384b0395534b3971e
zsolt-csondor <zsolt.csondor@gmail.com>
2018-03-29 17:15:20 +0100

    Update current scores, display active player

--- app.js
+++ app.js
@@ -17,10 +17,31 @@ document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
 
+  //Generate a dice roll
   var dice = Math.floor(Math.random() * 6 + 1);
 
+  //Displaying the roll in the middle
   var diceDOM = document.querySelector(".dice");
 
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
+
+  //Updating score IF the roll was not a 1
+  if(dice > 1) {
+    roundScore += dice;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  }
+  else {
+    roundScore = 0;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+
+  }
 });
