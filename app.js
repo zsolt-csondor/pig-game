fa8f99fee96491d831aeb599e6279e6d05475197
zsolt-csondor <zsolt.csondor@gmail.com>
2018-03-30 12:08:30 +0100

    next player function, hold button event listener

--- app.js
+++ app.js
@@ -24,24 +24,44 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   var diceDOM = document.querySelector(".dice");
 
   diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + dice + ".png";
+  diceDOM.src = "img/dice-" + dice + ".png";
 
-  //Updating score IF the roll was not a 1
+  //Updating score IF the roll was not a 1. Otherwise reset the current scores
+  //and change the active player.
   if(dice > 1) {
     roundScore += dice;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   }
   else {
-    roundScore = 0;
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-    document.querySelector(".dice").style.display = "none";
-
+    nextPlayer();
   }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //Add current score to global score and update the UI and check if the active
+  //player won
+  scores[activePlayer] += roundScore;
+  document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+  nextPlayer();
+
+
+});
+
+function nextPlayer() {
+  roundScore = 0;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+/* TODO refactoring:
+create an init() function and call that for the new game button and when the page loads.
+create separate functions for the roll dice and hold buttons instead of using anonymous functions
+save the target score into a variable instead of hardcoding it. Move the dice images into a separate folder.
+*/
