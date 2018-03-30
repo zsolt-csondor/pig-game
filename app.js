ef3ec37a5bd13a3e4985b6419e0dc0297fbe507a
zsolt-csondor <zsolt.csondor@gmail.com>
2018-03-30 12:43:41 +0100

    init functionand new game button

--- app.js
+++ app.js
@@ -2,18 +2,7 @@
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-//Initialization; Hiding the dice image before the first roll as well as
-//setting all the scores to 0
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
 
@@ -42,10 +31,44 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
   //player won
   scores[activePlayer] += roundScore;
   document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
-  nextPlayer();
 
-
+  if(scores[activePlayer] >= 10) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+    document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+  }
+  else {
+    nextPlayer();
+  }
 });
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+//Initialization; Hiding the dice image before the first roll as well as
+//setting all the scores to 0
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+}
 
 function nextPlayer() {
   roundScore = 0;
